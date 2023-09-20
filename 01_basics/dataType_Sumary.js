44ec789084ee25a4811e2cdfbaa184e365ca2f73
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-20 16:50:43 +0530

    change

---
+++ 01_basics/dataType_Sumary.js
@@ -0,0 +1,14 @@
+//Prmitive 7 data types : String, Bigint, Boolean, Number, Null, Undefined, Sysmbol
+
+//Reference (Non Primitive) : Array, Objects
+
+const heros = ["shaktiman", "spiderman", "Antman"];
+console.log(heros[1]);
+
+let hero = {
+    name : "Iron man",
+    Age : 35,
+    quality : "Save Life"
+} 
+
+console.log(hero.Age);
