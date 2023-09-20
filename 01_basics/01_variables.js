8b6b0e97ed1e1fe5ec2138d4a8c47453c9901b1f
shubham sutar <68117385+shubham-sutar@users.noreply.github.com>
2023-09-20 08:17:32 +0000

    changes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1001;
+console.log(accountId);
+
+
+// accountId = 1002; error bcoz this are constant.
+// console.log(accountId); 
+
+var accountName = "Shubham sutar";
+console.log(accountName);
+
+accountName = "Sayali"; // reassigned allowed
+console.log(accountName); // this are used for functional level
+
+let salary = 20000;
+console.log(salary);
+
+salary = 45000; //  this are only used for block level.
+console.log(salary); // reassigned allowed
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+let num = 23;
+console.log(typeof num); // number
+
+/*
+Data types in JavaScript
+
+number
+bigint
+string = " "
+boolean = true / false;
+null = standalone value;
+undefined
+symbole = unique
+
+*/
