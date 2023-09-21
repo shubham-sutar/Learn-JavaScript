6e85d413694436eb1c67fe52998509c8e2d6128d
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-21 11:30:56 +0530

    change

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -11,16 +11,20 @@ console.log(fullName.__proto__);
 console.log(fullName.charAt(8));
 console.log(fullName.indexOf('m'));
 
+console.log("------------- subdtring -----------");
 const newString = fullName.substring(2,8);
 console.log(newString);
 
+console.log("------------- sclice -----------");
 const anotherString = fullName.slice(-12,-9);
 console.log(anotherString);
 
+console.log("------------- trim -----------");
 let myString = "  Lets Practice  ";
 let myString1 = myString.trim();
 console.log(myString1);
 
+console.log("------------- replace & includes -----------");
 let url = "https://joshmarathi.com/new%20artical";
 let newUrl = url.replace('%20','-');
 console.log(newUrl);
@@ -28,5 +32,6 @@ console.log(url.replace('%20','-'));
 console.log(url.includes('joshmarathi')); //true
 console.log(url.includes('assets')); //false
 
+console.log("------------- split -----------");
 let lorem = "Welcome To The JavaScript World.";
 console.log(lorem.split(' '));
