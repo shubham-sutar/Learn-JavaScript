c7464923625d3d044c971092df42878e2112f4c5
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-22 11:14:32 +0530

    newFile

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,34 @@
+// Dates 
+
+let myDate = new Date()
+//console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toISOString);
+// console.log(myDate.toJSON);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+//let myCreatedDate = new Date(2023, 0, 23, 5, 3);
+//let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023");
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(myCreatedDate.getTime()/1000));
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getFullYear());
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+
+let newDate1 = newDate.toLocaleString('default',{
+    weekday : "long"
+});
+
+console.log(newDate1);
