25a28de08871d6083cbe915690e9abdf645d4580
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-22 13:14:01 +0530

    change

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,3 +5,36 @@ const myHeros = ["shubham","sayali","nita"];
 
 const myArr2 = new Array(12, 13, 15, 34);
 console.log(myArr2[1]);
+console.log(typeof myArr2);
+
+//methods
+console.log(myArr2.length);
+
+myArr2.push('shubh'); //adding value in last index
+myArr2.push('sayali');
+myArr2.push('rumani');
+
+//myArr2.pop(); //delete last index value
+
+myArr2.unshift(23); //adding value in first index
+myArr2.shift(); //delete value of first index
+console.log(myArr2.indexOf(34));
+ console.log(myArr2.includes(50)); //false
+
+ const numArr = new Array(23, 34, 56, 0);
+ const allArr = numArr.join(myArr);
+
+ console.log(allArr);
+ console.log(typeof allArr);
+
+ // slice and splice
+
+ const charArr = new Array('a','b','c','d');
+
+ 
+ console.log(charArr.slice(0,3)); //['a', 'b', 'c']
+ console.log(charArr); //['a', 'b', 'c', 'd']
+
+ console.log(charArr.splice(0,3)); //['a', 'b', 'c']
+ console.log(charArr); //['d']
+
