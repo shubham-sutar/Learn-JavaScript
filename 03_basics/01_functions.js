46a7cafd66d6667ab7e1d78d22d25a9d24451249
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-24 16:55:08 +0530

    new commit

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+ function myFunction(){
+    console.log("this is my function");
+ }
+
+ myFunction();
+
+ function addition(num1, num2){
+    let result = num1+num2;
+    //console.log(result);
+    return result;
+ }
+
+const result = addition(12,12);
+console.log(result);
