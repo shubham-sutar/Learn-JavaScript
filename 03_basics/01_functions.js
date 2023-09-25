cc0619618940ea352ee3d7398daa5b78aefc2e08
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-25 10:21:52 +0530

    update

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -4,6 +4,8 @@
 
  myFunction();
 
+//-----------------------------------------
+
  function addition(num1, num2){
     let result = num1+num2;
     console.log(result);
@@ -11,6 +13,8 @@
  }
 
  addition(12,12);
+
+//-----------------------------------------
 
  function mult(num1, num2){
     let result = num1 * num2;
@@ -20,12 +24,15 @@
  const result = mult(12,12);
  console.log('result is: ',result);
 
+ //-----------------------------------------
+
  function div(num1, num2){
     return num1 / num2;
  }
 
 console.log(div(12,12));
 
+//-----------------------------------------
 function loginUserMessage(username = "hey"){
     if(!username){
         console.log("please enter username");
@@ -36,3 +43,30 @@ function loginUserMessage(username = "hey"){
 
 let newUser = loginUserMessage();
 console.log(newUser);
+
+//------------------------------------------
+
+const user = {
+    username : "shubham",
+    price : 399
+}
+
+function myUser(obj){
+    console.log(`username is ${obj.username} and price is ${obj.price}`);
+}
+
+//myUser(user);
+myUser({
+    username : "sam",
+    price : 399
+})
+
+//-----------------------------------------
+
+const myArray = [100, 200, 300, 400];
+
+function myNewFunction(){
+    return myArray[1]
+}
+
+console.log(myNewFunction(myArray));
