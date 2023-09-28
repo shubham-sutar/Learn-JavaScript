4b588a170912e63c6ef27aa2dde62cdf09a21c79
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-28 19:19:42 +0530

    new

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,18 @@
+//if
+// comparison oprt. 
+// <, >, <=, >=, ==, ===,!=
+
+
+if(3 == 3){
+    console.log("true");
+}
+else{
+    console.log("false");
+} // true bcoz it check only value
+
+if(3 === '3'){
+    console.log("true");
+}
+else{
+    console.log("false");
+} // false bcoz it checks both value and type.
