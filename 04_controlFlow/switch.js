5ece0785bdaba706c8d9bd9036b67b24a09c2241
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-28 21:14:59 +0530

    update

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,21 @@
+// switch case
+
+const cards = 'masterCard';
+
+switch (cards) {
+    case 'rupay' :
+        console.log("NA");
+        break;
+
+    case 'visa' :
+        console.log("NA");
+        break;
+
+    case 'masterCard' :
+        console.log("Pay ur bill");
+        break;
+
+    default:
+        console.log("invalid");
+        break;
+}
