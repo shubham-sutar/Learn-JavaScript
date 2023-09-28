610f7aa7380d7d627b1d19438fa21b0f136c5cc0
shubham-sutar <shubhamsutar93@gmail.com>
2023-09-28 19:52:02 +0530

    update

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -16,3 +16,16 @@ if(3 === '3'){
 else{
     console.log("false");
 } // false bcoz it checks both value and type.
+
+//==================================
+
+const score = 200;
+
+if(score > 100){
+    const power = "fly";
+    console.log(`User power = ${power}`);
+}
+
+// console.log(`User power = ${power}`); //error bcz out of scope.
+
+//==================================
