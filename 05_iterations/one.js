ac32a43795c7df75ae43d875ea13a799ce76fa78
shubham-sutar <shubhamsutar93@gmail.com>
2023-10-02 19:06:04 +0530

    new update

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -6,3 +6,23 @@ for (let i = 0; i < num.length; i++) {
     const element = num[i];
     console.log(element);
 }
+
+for (let i = 2; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log( i + "x" + j + "=" + i * j );
+    }
+    console.log("\n");
+}
+
+
+for (let i = 0; i <= 20; i++){
+    if(i == 10){
+        console.log(`Detected 10`);
+        continue;
+    }
+    if (i == 18) {
+        console.log(`Detected 18`);
+        break;
+    }
+    console.log(`value of i is ${i}`);
+}
